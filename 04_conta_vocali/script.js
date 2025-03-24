17850398504eb6249ca09b9acc43e12beb2609ad
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-24 16:47:29 +0100

    [UPD] Es vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,8 +5,30 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function numVocali(x) {
+    const vocali = ['a', 'e', 'i', 'o', 'u']; 
+    let vocaliArray = [];
+  
+
+    for (let i = 0; i < x.length; i++) {
+      let letter = x[i];
+      letter = letter.toLowerCase();
+  
+      
+      if (vocali.includes(letter)) {
+        vocaliArray.push(letter);
+      }
+    }
+  
+    return vocaliArray.length;
+  }
+  
+
 
 // Invoca la funzione qui e stampa il risultato in console
+
+let num = numVocali(word)
+console.log(num)
 
 
 

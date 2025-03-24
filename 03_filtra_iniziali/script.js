1329753e34fc1bd66ef06360b72e3fa6f51fd2ba
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-24 16:14:38 +0100

    [UPD] Es iniziali array

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function startWord (word, letter) {
+    return word.charAt(0).toLowerCase() === letter.toLowerCase();
+}
+
+function namesByLetter(namesArray, letter) {
+    return namesArray.filter(name => startWord(name, letter));
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+let namesStartA = namesByLetter (names, 'A')
+console.log(namesStartA)
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]

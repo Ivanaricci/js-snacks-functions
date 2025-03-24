41f317bd8ca4f3b3c7a9f92f9af8d5dc3e82fb44
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-24 16:03:00 +0100

    [UPD] es iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,8 +5,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function iniziale (name){
+    return name.charAt(0)
+}
+
+function arrayIniziali (array) {
+    return array.map(iniziale)
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
+
+let inizialiNomi = arrayIniziali(names)
+console.log(inizialiNomi)
 
 
 
